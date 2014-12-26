329b490fece4ba8cd6de0df7303f2aa87e6dd1cc
psy_ipm <psy.ipm@gmail.com>
2014-12-26 02:19:25 +0200

    front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'application#index'
-  get '*path' => 'application#index'
+  resources :recipes
+
+  root 'recipes#index'
+  get '*path' => 'recipes#index'
 
 end

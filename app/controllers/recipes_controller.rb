bfa9b48f96a996eca0d25f7aa3a89199f569a452
psy.ipm <psy.ipm@gmail.com>
2015-01-13 02:42:03 +0200

    form for creating recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
+	# protect_from_forgery
 	
 	def index
 		
@@ -13,5 +14,13 @@ class RecipesController < ApplicationController
 		else
 			@recipes = Recipe.all
 		end
+	end
+
+	def new
+		
+	end
+
+	def create
+		@params = ActionController::Parameters.new(params[:recipe])
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   root 'recipes#index'
-  resources :recipes, only: [:index]
+  resources :recipes, only: [:index, :new, :create]
 
   post 'recipes/find'
 
